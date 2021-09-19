023d00b9db978455a9ee46efdbe8dba3c74c7422
Nishikantaray <nishikantaray1@gmail.com>
2021-09-19 18:00:42 +0530

    optimized

--- assets/js/script.js
+++ assets/js/script.js
@@ -2953,55 +2953,6 @@
 //     },
 //   },
 // });
-var thumbnails = document.getElementById("thumbnails")
-var imgs = thumbnails.getElementsByTagName("img")
-var main = document.getElementById("main")
-var counter=0;
-
-for(let i=0;i<imgs.length;i++){
-  let img=imgs[i]
-  
-  
-  img.addEventListener("click",function(){
-  main.src=this.src
-})
-  
-}
-
-var mySwiper = new Swiper ('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100,
-    initialSlide: 0,
-    //truewrapper adoptsheight of active slide
-    autoHeight: false,
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // delay between transitions in ms
-    autoplay: 5000,
-    autoplayStopOnLast: false, // loop false also
-    // If we need pagination
-    pagination: '.swiper-pagination',
-    paginationType: "bullets",
-    
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-    // And if we need scrollbar
-    //scrollbar: '.swiper-scrollbar',
-    // "slide", "fade", "cube", "coverflow" or "flip"
-    effect: 'slide',
-    // Distance between slides in px.
-    spaceBetween: 60,
-    //
-    slidesPerView: 2,
-    //
-    centeredSlides: true,
-    //
-    slidesOffsetBefore: 0,
-    //
-    grabCursor: true,
-  })        
+// Swiper Configuration
 
   
