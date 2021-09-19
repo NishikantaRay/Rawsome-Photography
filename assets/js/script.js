17a193f7655d622164fa7531e70a504ebfbde92e
Nishikantaray <nishikantaray1@gmail.com>
2021-09-19 15:20:35 +0530

    added photo in home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,57 @@ $(window).on('load resize',function(){menuFull()});(function($){$(window).on('lo
 var srcset=$image.data('jg-srcset');if($image.length>0&&srcset.length>0){var path,sizes=[],sizesTemp=[],urls=srcset.split(",");return currentPath}else{return currentPath}}}).on('jg.complete',function(e){$item.each(function(){$(this).on('mouseenter mouseleave',function(e){var $this=$(this),width=$this.width(),height=$this.height();var x=(e.pageX-$this.offset().left-(width/2))*(width>height?(height/width):1),y=(e.pageY-$this.offset().top-(height/2))*(height>width?(width/height):1);var dir_num=Math.round((((Math.atan2(y,x)*(180/Math.PI))+180)/90)+3)%4,directions=['top','right','bottom','left'];if(e.type==='mouseenter'){$this.removeClass(function(index,css){return(css.match(/(^|\s)hover-out-\S+/g)||[]).join(' ')});$this.addClass('hover-in-'+directions[dir_num])}
 if(e.type==='mouseleave'){$this.removeClass(function(index,css){return(css.match(/(^|\s)hover-in-\S+/g)||[]).join(' ')});$this.addClass('hover-out-'+directions[dir_num])}})})})}})})}(jQuery))
 $(".mob-nav").on("click",function(){$('.topmenu').removeClass('active-socials');});$(".description_tab").on("click",function(){$('.woocommerce-Tabs-panel').addClass('hide_info');$('#tab-description').removeClass('hide_info');$('.wc-tabs li').removeClass('active');$(this).addClass('active');});$(".additional_information_tab").on("click",function(){$('.woocommerce-Tabs-panel').addClass('hide_info');$('#tab-additional_information').removeClass('hide_info');$('.wc-tabs li').removeClass('active');$(this).addClass('active');});$(".reviews_tab").on("click",function(){$('.woocommerce-Tabs-panel').addClass('hide_info');$('#tab-reviews').removeClass('hide_info');$('.wc-tabs li').removeClass('active');$(this).addClass('active');});$(".value label").on("click",function(){$('.all_price').html($(this).data("price"));})})(jQuery,window,document);
+
+
+
+
+// $(document).ready(function() {
+//     // Swiper: Slider
+//         new Swiper('.swiper-container', {
+//             loop: true,
+//             nextButton: '.swiper-button-next',
+//             prevButton: '.swiper-button-prev',
+//             slidesPerView: 3,
+//             paginationClickable: true,
+//             spaceBetween: 20,
+//             breakpoints: {
+//                 1920: {
+//                     slidesPerView: 3,
+//                     spaceBetween: 30
+//                 },
+//                 1028: {
+//                     slidesPerView: 2,
+//                     spaceBetween: 30
+//                 },
+//                 480: {
+//                     slidesPerView: 1,
+//                     spaceBetween: 10
+//                 }
+//             }
+//         });
+//     });
+    var Swipes = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        breakpoints: {
+                            1920: {
+                                slidesPerView: 3,
+                                spaceBetween: 30
+                            },
+                            1028: {
+                                slidesPerView: 2,
+                                spaceBetween: 30
+                            },
+                            480: {
+                                slidesPerView: 1,
+                                spaceBetween: 10
+                            }
+                        }
+    });
