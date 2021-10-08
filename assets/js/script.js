048b88b066718e85c04f80d2db6750545c26a8b1
Nishikantaray <nishikantaray1@gmail.com>
2021-10-08 23:25:31 +0530

    Added pic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2982,3 +2982,5 @@ $('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
 $('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
 $('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
 $('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
+
+
